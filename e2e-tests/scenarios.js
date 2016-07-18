61525364eb4022cb1c84d7522fed001b9af760b7
George Pavlovych <pavlheo@hotmail.com>
2016-07-18 07:50:29 +0300

    protractor tests fixed

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -3,29 +3,58 @@
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
 describe('my app', function() {
+  var firstNameInput = element(by.id('firstName'));
+  var lastNameInput = element(by.id('lastName'));
   var usernameInput = element(by.id('username'));
   var passwordInput = element(by.id('password'));
+
   var loginButton = element(by.buttonText("Login"));
-  it('should automatically redirect to /login when location hash/fragment is empty and not logged in, then we log im', function() {
-    browser.get('index.html');
+  var registerLink = element(by.css("a[href='#/register']"));
+  var registerButton = element(by.css("input[value='Register']"));
+  var now = new Date();
+  var firstNameText = 'UAT First Name '+now;
+  var lastNameText = 'UAT Last Name '+now;
+  var usernameText = 'uat_user'+now;
+  var passwordText = 'uat_password'+now;
+
+  it('should automatically redirect to /login when location hash/fragment is empty and not logged in, then we register and log in', function() {
+    browser.get('');
     expect(browser.getLocationAbsUrl()).toMatch("/login");
-    usernameInput.sendKeys('pavlheo1');
-    passwordInput.sendKeys('1');
+    registerLink.click();
+    expect(browser.getLocationAbsUrl()).toMatch("/register");
+    expect(registerButton.isEnabled()).toBe(false);
+    firstNameInput.sendKeys(firstNameText);
+    expect(registerButton.isEnabled()).toBe(false);
+    lastNameInput.sendKeys(lastNameText);
+    expect(registerButton.isEnabled()).toBe(false);
+    usernameInput.sendKeys(usernameText);
+    expect(registerButton.isEnabled()).toBe(false);
+    passwordInput.sendKeys(passwordText);
+    expect(registerButton.isEnabled()).toBe(true);
+    registerButton.click();
+    expect(browser.getLocationAbsUrl()).toMatch("/login");
+    expect(loginButton.isEnabled()).toBe(false);
+    usernameInput.sendKeys(usernameText);
+    expect(loginButton.isEnabled()).toBe(false);
+    passwordInput.sendKeys(passwordText);
+    expect(loginButton.isEnabled()).toBe(true);
     loginButton.click();
     expect(browser.getLocationAbsUrl()).toMatch("/gameTicTacToe");
   });
 
 
-  /*describe('gameTicTacToe', function() {
+  describe('gameTicTacToe', function() {
 
     beforeEach(function() {
-      browser.get('index.html#/gameTicTacToe');
+      browser.get('#/gameTicTacToe');
     });
 
 
     it('should render gameTicTacToe when user navigates to /gameTicTacToe', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for gameTicTacToe/);
+      expect(element.all(by.css('[ng-view] table tr')).count()).
+        toEqual(3);
+      expect(element.all(by.css('[ng-view] table tr td')).count()).
+          toEqual(9);
     });
 
   });
@@ -34,14 +63,14 @@ describe('my app', function() {
   describe('gameDots', function() {
 
     beforeEach(function() {
-      browser.get('index.html#/gameDots');
+      browser.get('#/gameDots');
     });
 
 
     it('should render gameDots when user navigates to /gameDots', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for gameDots/);
+      expect(element.all(by.css('[ng-view] canvas#myCanvas')).count()).
+        toEqual(1);
     });
 
-  });*/
+  });
 });
